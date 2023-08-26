7dc40e6f092aab38ce44827cb92db41ddbe12f29
Lucas Ortiz <lucas.ortiz@nyu.edu>
2023-08-26 13:36:07 -0400

    Initial Commit

---
+++ src/test/java/nyc/pikaboy/snowflake/SnowflakeApplicationTests.java
@@ -0,0 +1,13 @@
+package nyc.pikaboy.snowflake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowflakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
